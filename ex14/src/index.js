9bca1adc08bbf31d5c457716073396f9cf996c98
EmilJulevic <emiljulevic@live.com>
2021-03-15 22:21:35 +0100

    Adding ex14 folder

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -3,6 +3,6 @@ function convertStrToInt(str) {
 }
 
 console.log(convertStrToInt("44"));
-console.log(convertStrToInt("27"))
-console.log(convertStrToInt("JeffBezos"))
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
 module.exports = convertStrToInt;
